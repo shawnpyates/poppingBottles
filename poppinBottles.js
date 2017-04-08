a9f7330a5fa557cedff0d90bee5bb3a1420341f9
Shawn Yates <yates.shawnp@gmail.com>
2017-04-08 22:58:42 +0000

    Added comments, output data, and made small adjustments to control flow

--- poppinBottles.js
+++ poppinBottles.js
@@ -1,71 +1,85 @@
+// NOTE TO CLARIFY DEFINITIONS FOR MY COMMENTS:
+// (1) "POPS" REFERS TO FULL BOTTLES OF POP.
+// (2) "BOTTLES" REFERS TO EMPTY POP BOTTLES
+
 var dollars = {neededForPop: 2};
-var bottles = {neededForPop: 2}; // bottles.amount: 1
-var caps = {neededForPop: 4};  // caps.amount: 1
+var bottles = {neededForPop: 2, extraPopsYielded: 0, amount: 0};
+var caps = {neededForPop: 4, extraPopsYielded: 0, amount: 0};
 var totalPopsConsumed = 0;
 var popsAtMoment = 0;
-var recursions = 0;
-var resetToZero = false;
-var returnString = "";
-
-// After 1st Run
-// bottles.amount: 1
-// caps.amount: 1
-// popsAtMoment = 3
-// totalPopsConsumed = 5
-
-// On 2nd Run
-// bottles.amount: 4
-// caps.amount: 4
-// totalsPopsConsumer = 8
-// popsAtMoment = 0
-// popsAtMoment = 2
-// bottles.amount:
-
+var reset = false;
+var arg = process.argv[2];
 
 function getTotalPopYield (dollarsSpent) {
+  // get initial set of pops by buying them with dollars
   popsAtMoment += getPops(dollars, dollarsSpent);
+  // extract bottles and caps from the pops
   setAmount(bottles, popsAtMoment);
   setAmount(caps, popsAtMoment);
+  // track the total pop consumed, store in variable that stays in the 'background'
   totalPopsConsumed += popsAtMoment;
+  // reset the number of pops in the 'foreground'
   popsAtMoment = 0;
-  if (bottles.amount >= 2) {
-    convertToPopAndReset(bottles);
+  // if we have enough bottles or caps to buy more pop, we spend them and reset their values
+  if (bottles.amount >= bottles.neededForPop) {
+    exchangeForPopAndReset(bottles);
   }
-  if (caps.amount >= 4){
-    convertToPopAndReset(caps);
+  if (caps.amount >= caps.neededForPop){
+    exchangeForPopAndReset(caps);
   }
+  // if we still have more pop left in the 'foreground', run through this function again
   if (popsAtMoment > 0) {
-    recursions++;
     getTotalPopYield(0);
   }
-  return totalPopsConsumed;
+  // after all pops have been consumed and we don't have enough bottles/caps to buy more...
+  // return easy-to-read string that shows key data points
+  var returnString = "*************************************************" + "\n";
+  returnString += "Total pops consumed: " + totalPopsConsumed + "\n";
+  returnString += "Total pops obtained through bottle exchange: " + bottles.extraPopsYielded + "\n";
+  returnString += "Total pops obtained through bottle cap exchange: " + caps.extraPopsYielded + "\n";
+  returnString += "Bottles remaining: " + bottles.amount + "\n";
+  returnString += "Caps remaining: " + caps.amount + "\n";
+  returnString += "*************************************************";
+  return returnString;
 }
 
-
+// buy pop (initially with dollars, and then later with bottles/caps)
 function getPops (paymentType, amount) {
   return Math.floor(amount / paymentType.neededForPop);
 }
 
+// get remaining bottles/caps after the maximum amount has been exchanged for pop
 function getRemainder (paymentType, amount) {
   return amount % paymentType.neededForPop;
 }
 
 function setAmount (paymentType, amount) {
-  if (recursions > 0 && !resetToZero) {
-    paymentType.amount += amount;
-  } else {
+  // (1) when we are exchanging bottles/caps for pop, we are giving them up, which means...
+  // we have to reset their respective values to the number that remains after the exchange
+  // (2) when we are extracting bottles/caps from pop, we are adding them to the...
+  // current amount
+  if (reset) {
     paymentType.amount = amount;
+  } else {
+    paymentType.amount += amount;
   }
 }
 
-function convertToPopAndReset(paymentType) {
-  resetToZero = true;
+// exchange bottles/caps for pop and recalculate their values
+function exchangeForPopAndReset(paymentType) {
+  // set 'reset' to true to account for bottles/caps given up upon exchange
+  reset = true;
+  // exchange bottles/caps for pop
   popsAtMoment += getPops(paymentType, paymentType.amount);
+  // keep track of how many total bottles/caps we exchanged for pop
+  paymentType.extraPopsYielded += getPops(paymentType, paymentType.amount);
+  // reset amount of bottles/caps
   setAmount(paymentType, getRemainder(paymentType, paymentType.amount));
-  resetToZero = false;
+  // reset 'reset' to false because "setAmount" is later going to be used to add values...
+  // rather than reset them
+  reset = false;
 }
 
-
-console.log(getTotalPopYield(10));
+console.log(getTotalPopYield(arg));
 
 
---
+++ readme.txt
@@ -0,0 +1,27 @@
+Problem:
+Poppin' Bottles
+You've just been hired at Lighthouse Markets, a reputable (and thoroughly fictional) grocery store chain.
+One of the primary features of Lighthouse Markets is their recycling program on pop bottles. Here is how
+the program works:
+
+For every two empty bottles, you can get one free (full) bottle of pop
+For every four bottle caps, you can get one free (full) bottle of pop
+Each bottle of pop costs $2 to purchase
+Given these parameters, write a program so that you can figure out how many total bottles of pop can be
+redeemed given a customer investment.
+
+Task 1
+Figure out the algorithm that will calculate this. For example, given a $20 investment, the customer will get 10 bottles of pop. That gives a supply of both bottles and bottle caps that can be used to redeem for further bottles, which will then produce a further supply for recycling.
+
+Task 2
+Have your program accept an argument (argv) that will allow the user to specify the amount (in dollars)
+that the customer is going to spend. The node script will then calculate the total number of bottles that the customer will receive.
+
+Task 3
+Enhance the output of your program so that once the amount has been given, it provides a breakdown of how many bottles were purchased, how many were obtained through bottle recycling, and how many were obtained
+through bottle cap recycling.
+
+Task 4
+Add to the output, so that the program will tell the customer how many bottles and bottle caps they will
+have left over. We have to upsell the customer on buying more pop after all!
+
